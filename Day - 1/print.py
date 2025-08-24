554eb9d9e78cfe84ab3a7649e977b22876194547
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-08-25 00:11:15 +0600

    Print Modifiers

--- Day - 1/print.py
+++ Day - 1/print.py
@@ -1,2 +1,15 @@
 print("Hello World!")
 print("Let's Start")
+
+
+
+print("1. MIx 500g of Flour, 10g Yeast and 300ml Water in a bowll")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+
+#print modifiers
+print('She said: "Hello" and then left.')
+print("She said: \"Hello\" and then left.")
