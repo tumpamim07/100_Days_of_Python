dcb7bd15efd3df92d19829a3fafffc371c80d032
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-08-26 22:59:14 +0600

    Input Function & Length Function

---
+++ Day - 1/input.py
@@ -0,0 +1,21 @@
+input("What is your name?") 
+
+print("Hello " + input("What is Your Name?") + "!")
+
+
+
+# len function to measure length
+numofLetters = len("Coder")
+print(numofLetters)
+
+num = len("Parenthesis")
+print(num)
+
+
+# program that calculates and outputs the number of characters in any name
+number = len(input())
+print(number) # User types a name → len() counts it → result stored in 'number'
+
+
+print(len(input()))
+
