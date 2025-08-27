83a5c1812fff7e5bc80bdae4a3798b2e7704b301
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-08-27 23:05:31 +0600

    Variable & Problem Solving

---
+++ Day - 1/variable.py
@@ -0,0 +1,23 @@
+name = "Yellow"
+print(name)
+
+name = "Green"
+print(name)
+
+
+name = input("What is Your Name?")
+length = len(name)
+print(length)
+
+
+#a program that switches the values stored in the variables a and b
+
+a = input()
+b = input()
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
