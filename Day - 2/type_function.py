2b1cfa0f1e85d9e86ed62cad341cb673fae89dc0
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-08-28 23:59:13 +0600

    Type Error, Type Checking and Type Conversion

---
+++ Day - 2/type_function.py
@@ -0,0 +1,19 @@
+# num_char = len(input("What is your name?"))
+# print(type(num_char))
+
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+
+a = 100
+print(type(a))
+
+
+b = str(100)
+print(type(b))
+
+c = float(123)
+print(type(c))
+
+print(70 + float("100.75"))
+print(str(100) + str(25.5))
