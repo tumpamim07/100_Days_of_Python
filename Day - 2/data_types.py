db9fb0a4e771f9b9c5cb2a25851b5b707f15e703
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-08-28 23:48:32 +0600

    Data Type

---
+++ Day - 2/data_types.py
@@ -0,0 +1,22 @@
+#primitive data type
+
+
+#subscript
+print("Hello"[4] + "World"[3])
+
+name = "Coders Color"
+print(name[2] + name [7])
+
+#integer
+print(253+5)
+
+
+#Float
+print(3.154)
+
+#boolean
+True
+False
+
+#string
+print("Hello")
