427e24b3636a0d2f9e7f433b451c00e932610113
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-09-01 00:04:40 +0600

    Mathematical Operations

---
+++ Day - 2/operators.py
@@ -0,0 +1,21 @@
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+2 ** 3
+
+"""
+PEMDASLR
+Parentheses()
+Exponents ** (Power)
+Multiplication *
+Division /
+Addition +
+Subtraction -
+Left
+Right 
+
+"""
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
