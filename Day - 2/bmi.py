2f5e5057a52e26f81a3c881cab513c9ff1458add
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-09-02 00:20:27 +0600

    BMI Calculator

---
+++ Day - 2/bmi.py
@@ -0,0 +1,13 @@
+#If the user enters height as 150 instead of 1.50, what will happen to the BMI calculation?
+
+weight = input("Weight: ")
+height = input("Height: ")
+
+
+weight_as_int = int(weight)
+height_as_float = float(height)
+
+bmi = weight_as_int / (height_as_float ** 2)
+
+bmi_as_int = int(bmi)
+print(bmi_as_int)
