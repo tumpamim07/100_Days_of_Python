5a3c0904674c1a2345f489dd28a536571affed9a
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-11-30 21:45:18 +0600

    Primitive Data Types

--- Day - 2/data_types.py
+++ Day - 2/data_types.py
@@ -3,20 +3,25 @@
 
 #subscript
 print("Hello"[4] + "World"[3])
+print("Tangerine"[2])
+
+# string
+print("123" + "345")     # string concatenation → "123345"
 
 name = "Coders Color"
 print(name[2] + name [7])
 
-#integer
+#integer = whole number
 print(253+5)
+print(3+5)
+
+# large interger
+print(123_456_789)
 
 
 #Float
 print(3.154)
 
 #boolean
-True
-False
-
-#string
-print("Hello")
+print(True)
+print(False)
