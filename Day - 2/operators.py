8a3cf883d40ac77099528763f9579522fe60fa79
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-12-01 22:26:27 +0600

    Mathematical operators

--- Day - 2/operators.py
+++ Day - 2/operators.py
@@ -1,8 +1,11 @@
-3 + 5
-7 - 4
-3 * 2
-6 / 3
-2 ** 3
+print(3 + 5)
+print(7 - 4)
+print(3 * 2)
+print(6 / 3)
+print(6 // 3)  #It is not preferable, as this can cause problems in the future.
+print(5 / 3)
+print(5 // 3)
+print(2 ** 3)
 
 """
 PEMDASLR
