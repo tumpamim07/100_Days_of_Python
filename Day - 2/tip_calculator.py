2b5f6e0839a04d069fd50153da3cac0f9963e4a9
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-12-03 21:32:57 +0600

    Tip Calculator

--- Day - 2/tip_calculator.py
+++ Day - 2/tip_calculator.py
@@ -11,15 +11,15 @@ Then, it should calculate and print how much each person should pay.
 
 
 print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill?\n$"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15\n"))
+people = int(input("How many people to split the bill?\n"))
 
-bill=float(input("What was the toatl bill? $"))  # total bill
-tips = int(input("How much tip would you like to give? 10, 12, or 15?"))
-split_bill = int(input("How many people to split the bill?"))
-
-total_bill = bill + (bill * (tips / 100))
-bill_per_person = round(total_bill / split_bill, 2)
-bill_per_person = "{:.2f}".format(bill_per_person) # Format the result to always show 2 decimals (e.g. 27.50)
-print(f"Each person should pay: ${bill_per_person}") 
+bill_will_tip = bill*(tip/100)
+total_bill = bill + bill_will_tip
+bill_per_person = total_bill / people
+final_bill = round(bill_per_person, 2)
+print(f"Each person should pay: ${final_bill}")
 
 
 # :.2f means "fixed-point number with 2 decimals"
