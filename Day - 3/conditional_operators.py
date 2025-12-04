13b887d567a285b7fbd722eb6cccd185ddc54c2e
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-12-04 22:38:00 +0600

    Conditional Operator

---
+++ Day - 3/conditional_operators.py
@@ -0,0 +1,23 @@
+print("Welcome to the rollercoaster!")
+
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+else: 
+    print("Sorry you have to grow taller before you can ride.")
+
+
+"""
+Greater than >
+Less than <
+Greater than or equal to >=
+Less than or equal to <=
+Equal to ==
+Not equal to !=
+
+"""
+
+# = (single equal) Used to assign a value.
+# == (double equal) Used to compare two values.
+
