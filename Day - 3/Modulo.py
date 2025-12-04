bd62a6a8152630b8c035f23312e0bd2a59ce0d39
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-12-04 22:47:15 +0600

    Modulo

---
+++ Day - 3/Modulo.py
@@ -0,0 +1,10 @@
+"""
+Modulo Operator = % (It gives you the remainder after dividing two numbers.)
+"""
+
+number = int(input("What is the number you want to check? "))
+
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
