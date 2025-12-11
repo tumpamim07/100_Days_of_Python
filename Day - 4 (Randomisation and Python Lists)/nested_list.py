4827543ceab523c246f9894657508e0165d43fd4
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-12-11 18:47:27 +0600

    Nested List

---
+++ Day - 4 (Randomisation and Python Lists)/nested_list.py
@@ -0,0 +1,18 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+
+number_of_fruits = len(fruits)
+print(fruits[number_of_fruits-1])       # printing the last item using index (length - 1)
+
+
+vegetables = ["Tomatoes", "Spinach", "Kale", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]      # nested list combining fruits + vegetables
+print(dirty_dozen[1][2])                # prints the 3rd item of the vegetables list ("Kale")
+
+
+
+
+
+
+
+
