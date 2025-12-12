64e853cc500a5e9c62323644a633d769f968e2d0
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-12-12 21:36:33 +0600

    Loop with Python List

---
+++ Day - 5 (Python Loops)/loop_with_list.py
@@ -0,0 +1,5 @@
+fruits = ["Apple", "Peache", "Pear"]    # Creating a list that stores three fruit names
+for fruit in fruits:            # Looping through each fruit inside the list one by one
+    print(fruit)                # Showing the current fruit on the screen
+    print(fruit + " pie")       # Showing the fruit name combined with the word "pie"
+print(fruits)                   # Showing the entire original list after the loop is done
