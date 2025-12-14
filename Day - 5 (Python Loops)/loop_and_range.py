9bd582da652845615f4ed9d0266c60657c555774
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-12-14 12:03:14 +0600

    For Loop and The Range Function

---
+++ Day - 5 (Python Loops)/loop_and_range.py
@@ -0,0 +1,13 @@
+#Range
+for number in range(1,11):
+    print(number)
+
+for number2 in range(1,11,2):
+    print(number2)
+
+    
+
+sum = 0
+for number3 in range(1,101):
+    sum += number3
+print(sum)
