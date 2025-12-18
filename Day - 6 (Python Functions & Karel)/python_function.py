44c739fc79c22535164662450a075543ea1087f6
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-12-18 21:51:25 +0600

    Defining and Calling Python Functions

---
+++ Day - 6 (Python Functions & Karel)/python_function.py
@@ -0,0 +1,8 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+def my_function():          # Defines a function named my_function
+    print("Hello")
+    print("Bye")
+my_function()           # Calls the function and executes its code
