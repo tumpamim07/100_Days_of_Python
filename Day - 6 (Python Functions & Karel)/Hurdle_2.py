6eee3758e16a21ad54dfc5a16a889ee557ab460d
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-12-22 18:42:16 +0600

    Hurdle 2 using While Loop

---
+++ Day - 6 (Python Functions & Karel)/Hurdle_2.py
@@ -0,0 +1,18 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def turn_around():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+# while at_goal()  != True:
+while not at_goal():
+    turn_around()
